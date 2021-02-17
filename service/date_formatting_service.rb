4cf009db35355505d23c5daab9914344299a734f
NazarovSV <badger.lock@yandex.ru>
2021-02-17 13:53:58 +0300

    revert

--- service/date_formatting_service.rb
+++
@@ -1,8 +0,0 @@
-class DateFormattingService
-  AUTHORIZED_FORMAT = { year: '%Y', month: '%m', day: '%d', hour: '%H', minute: '%M', second: '%S' }
-
-  def get_date(formats)
-    time_format = formats.map { |format| AUTHORIZED_FORMAT[format] }.join('/')
-    Time.now.strftime(time_format)
-  end
-end
